4146bb363d0a58c2dd193841cfeb773322898e3c
MisterCatLady <135910738+MisterCatLady@users.noreply.github.com>
2025-10-18 16:46:22 -0500

    Add files via upload

---
+++ startercode/include/Tree.hpp
@@ -0,0 +1,190 @@
+#include <memory>
+#include <functional>
+#include <cassert>
+#include <iostream>
+#include <initializer_list>
+#include <utility>
+
+template <class T>
+class Tree
+{
+private:
+	struct Node
+	{
+		Node(std::shared_ptr<const Node> lft,
+			 T val,
+			 std::shared_ptr<const Node> rgt) 
+			: _lft(lft), _val(val), _rgt(rgt) {}
+			 std::shared_ptr<const Node> _lft;
+			 T _val;
+			 std::shared_ptr<const Node> _rgt;
+	};
+	explicit Tree(std::shared_ptr<const Node> node) : _root(node) {}
+public:
+	Tree() : _root(nullptr) {}
+	Tree(Tree const& other) = default;
+	Tree& operator=(Tree const& other) = default;
+	~Tree() = default;
+	Tree(Tree&& other) = default;
+	Tree& operator=(Tree&& other) = default;
+
+	Tree(Tree lft, T val, Tree rgt) 
+		: _root(std::make_shared<Node>(lft._root, val, rgt._root)) {}
+
+	Tree(std::initializer_list<T> init)
+	{
+		Tree t;
+		for (T v : init)
+		{
+			t = t.insert(v);
+			_root = t._root;
+		}
+	}
+
+	bool isEmpty() const
+	{
+		return !_root;
+	}
+	size_t size() const
+	{
+		if (isEmpty()) return 0;
+		return 1 + left().size() + right().size();
+	}
+	T root() const
+	{
+		assert(!isEmpty());
+		return _root->_val;
+	}
+	Tree left() const
+	{
+		assert(!isEmpty());
+		return Tree(_root->_lft);
+	}
+	Tree right() const
+	{
+		assert(!isEmpty());
+		return Tree(_root->_rgt);
+	}
+
+	template <typename Compare = std::less<T>>
+	Tree insert(T x, const Compare& comp = Compare()) const
+	{
+		if (isEmpty())
+			return Tree(Tree(), x, Tree());
+		T y = root();
+		if (comp(x, y))
+			return Tree(left().insert(x, comp), y, right());
+		else if (comp(y, x))
+			return Tree(left(), y, right().insert(x, comp));
+		else
+			return *this;
+	}
+
+	template <typename Compare = std::less<T>>
+	bool member(T x, const Compare& comp = Compare()) const
+	{
+		if (isEmpty()) { return false; }
+		T y = root();
+		if (comp(x, y)) { return left().member(x, comp); }
+		else if (comp(y, x)) { return right().member(x, comp); }
+		else { return true; }
+	}
+
+	template <typename Compare = std::less<T>>
+	bool find(T x, Tree& subtreeWhereFound, const Compare& comp = Compare()) const
+	{
+		if (isEmpty())
+		{
+			subtreeWhereFound = Tree();
+			return false;
+		}
+		T y = root();
+		if (comp(x, y)) { return left().find(x, subtreeWhereFound, comp); }
+		else if (comp(y, x)) { return right().find(x, subtreeWhereFound, comp); }
+		else { subtreeWhereFound = *this; return true; }
+	}
+
+	void preorder(std::function<void(T)> visit) const
+	{
+		if (isEmpty())
+			return;
+		T contents = root();
+		visit(contents);
+		left().preorder(visit);
+		right().preorder(visit);
+	}
+	void inorder(std::function<void(T)> visit) const
+	{
+		if (isEmpty())
+			return;
+		left().inorder(visit);
+		T contents = root();
+		visit(contents);
+		right().inorder(visit);
+	}
+	void postorder(std::function<void(T)> visit) const
+	{
+		if (isEmpty())
+			return;
+		left().postorder(visit);
+		right().postorder(visit);
+		T contents = root();
+		visit(contents);
+	}
+	template <typename NodeType>
+	void treeTraversal(std::function<void(NodeType)> visit) const
+	{
+		if (isEmpty()) return;
+		left().treeTraversal<NodeType>(visit);
+		visit(root());
+		right().treeTraversal<NodeType>(visit);
+	}
+
+	template <typename NodeType>
+	static void printNode(NodeType& nodeValue)
+	{
+		std::cout << nodeValue << " ";
+	}
+	
+
+private:
+	std::shared_ptr<const Node> _root;
+};
+
+template <typename T>
+int treeHeight(const Tree<T>& t) 
+{
+	if (t.isEmpty()) return 0;
+
+	int leftHeight = treeHeight(t.left());
+	int rightHeight = treeHeight(t.right());
+
+	if (leftHeight > rightHeight)
+		return 1 + leftHeight;
+	else
+		return 1 + rightHeight;
+}
+template <typename T, typename Func>
+void visitLevel(const Tree<T>& t, int level, Func visit)
+{
+	if (t.isEmpty()) return;
+	if (level == 0)
+	{
+		visit(t.root());
+	}
+	else {
+		visitLevel(t.left(), level - 1, visit);
+		visitLevel(t.right(), level - 1, visit);
+	}
+}
+
+template <typename T, typename Func>
+void levelTraversal(const Tree<T>& t, Func visit)
+{
+	int h = treeHeight(t);
+	for (int level = 0; level < h; ++level)
+	{
+		visitLevel(t, level, visit);
+	}
+}
+
